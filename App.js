66ae4965e41a71a152bdf6c683dc093a5c670bdb
keystristian23-design <keystristian23@gmail.com>
2025-07-27 19:01:05 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,3 @@
-
 import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>AlphaBlade Control Dashboard</h1>
-      <button onClick={() => alert('Launching AlphaBlade FX')}>Launch AlphaBlade FX</button>
-      <button onClick={() => alert('Launching Pump Sniper')}>Launch Pump Sniper</button>
-    </div>
-  );
-}
-
+function App() { return <div>AlphaBlade Bot Control</div>; }
 export default App;
